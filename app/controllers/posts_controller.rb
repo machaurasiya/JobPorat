324429ad02d31c72b7fb14ac167cf9ab42f4b184
machaurasiya <machaurasiya@bestpeers.in>
2025-01-08 13:02:37 +0530

    add destroy action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,34 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+
+  load_and_authorize_resource
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
+
+      # Rails.logger.debug @post.errors.full_messages
+      Rails.logger.debug "Params: #{params.inspect}"
+
+      @post = Post.find(params[:id])
   end
 
   def new
     @post = Post.new
+    authorize! :create, @post #if authorised then create a post
+    render :new
+
   end
 
   def create
-    
-    @post = Post.new(params_post)
+    # debugger
+    @post = Post.new(post_params)
     if @post.save
       redirect_to @post
     else
 
-      # Rails.logger.debug @post.errors.full_messages
       render :new
     end
   end
@@ -30,16 +40,24 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
 
-    if @post.update(params_post)
+    if @post.update(post_params)
       redirect_to @post
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to post_path, status: :see_other
+  end
+
+
 
   private
-  def params_post
-    params.require(:post).permit(:title, :description, :user_id,:salary, :location)
+  def post_params
+    params.require(:post).permit(:user_id, :title, :description, :salary, :location)
   end
 end
